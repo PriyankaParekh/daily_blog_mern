e708504e2f959bc7dc02f1eaa301460da76d85e3
PriyankaParekh <87693278+PriyankaParekh@users.noreply.github.com>
2023-05-28 16:33:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ require('dotenv').config()
     //require("./db/config");
 const PORT = process.env.PORT || 5000
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use(cors());
 
 //app.get('/', (req, res) => {
